064e90036499637e572398a71df1ec1e952f355c
bhan <01169172@pw.edu.pl>
2025-10-29 17:20:16 +0100

    Improve GTZAN classifier model.

---
+++ datasets.py
@@ -0,0 +1,67 @@
+import torch
+import os
+import torchaudio
+import torch.nn.functional as F
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class GTZANImageDataset(Dataset):
+    def __init__(self, root):
+        self._samples = []
+
+        genres_names = sorted([
+            "blues", "classical", "country", "disco", "hiphop",
+            "jazz", "metal", "pop", "reggae", "rock"
+        ])
+        self._genre_to_idx = {genre: i for i, genre in enumerate(genres_names)}
+
+
+        genres = os.listdir(root)
+        for genre in genres:
+            genre_path = os.path.join(root, genre)
+            for file in os.listdir(genre_path):
+                if file.endswith(".png"):
+                    self._samples.append((os.path.join(genre_path, file), genre))
+
+    def __len__(self):
+        return len(self._samples)
+
+    def __getitem__(self, index):
+        path, label = self._samples[index]
+        label = self._genre_to_idx[label]
+        image = torch.from_numpy(np.array(Image.open(path).convert("RGB"))).permute(2, 0, 1).float()
+        return image, label
+
+
+class GTZANWaveDataset(Dataset):
+    def __init__(self, root):
+        self._samples = []
+
+        genres_names = sorted([
+            "blues", "classical", "country", "disco", "hiphop",
+            "jazz", "metal", "pop", "reggae", "rock"
+        ])
+        self._genre_to_idx = {genre: i for i, genre in enumerate(genres_names)}
+
+
+        genres = os.listdir(root)
+        for genre in genres:
+            genre_path = os.path.join(root, genre)
+            for file in os.listdir(genre_path):
+                if file.endswith(".wav"):
+                    self._samples.append((os.path.join(genre_path, file), genre))
+
+    def __len__(self):
+        return len(self._samples)
+
+    def __getitem__(self, index):
+        TARGET_LEN = 3309
+        path, label = self._samples[index]
+        label = torch.tensor(self._genre_to_idx[label])
+        label = torch.nn.functional.one_hot(label, 10).to(torch.float32)
+        waveform, sample_rate = torchaudio.load(path)
+        mel_spectogram = torchaudio.transforms.MelSpectrogram(sample_rate, n_fft=1024)(waveform)
+        mel_spectogram = F.pad(mel_spectogram, (TARGET_LEN - mel_spectogram.size(2), 0))
+        return mel_spectogram, label
---
+++ model.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from datasets import GTZANWaveDataset
+
+class GTZANClassifier(nn.Module):
+    def __init__(self):
+        super(GTZANClassifier, self).__init__()
+
+        self.features = nn.Sequential(
+         nn.Conv2d(1, 64, kernel_size=3, padding=1),
+         nn.ReLU(),
+         nn.Conv2d(64, 64, kernel_size=3, padding=1),
+         nn.ReLU(),
+         nn.Conv2d(64, 64, kernel_size=3, padding=1),
+         nn.ReLU(),
+         nn.Conv2d(64, 16, kernel_size=3, padding=1),
+         nn.ReLU(),
+         nn.MaxPool2d(4),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(16 * 4 * 4 * 1654, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, 10),
+        )
+
+    def forward(self, x):
+
+        x = self.features(x)
+        x = self.classifier(x)
+
+        return x
+
+
+if __name__ == "__main__":
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using: {device.type}")
+    model = GTZANClassifier().to(device)
+    crtierion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters())
+
+
+    X = GTZANWaveDataset("GTZAN/genres_original")
+    X_train, X_test = torch.utils.data.random_split(X, [0.8, 0.2])
+
+    train_dataloader = torch.utils.data.DataLoader(
+        X_train,
+        batch_size=10,
+        shuffle=True,
+        pin_memory=(device == "cuda"),
+        num_workers=(4 if device == "cuda" else 0),
+    )
+
+    targets_dataloader = torch.utils.data.DataLoader(
+        X_test,
+        batch_size=10,
+        shuffle=True,
+        pin_memory=(device == "cuda"),
+        num_workers=(4 if device == "cuda" else 0),
+    )
+
+    N_ITERATIONS = 2
+
+    for i in range(N_ITERATIONS):
+        j=1
+        for spectogram, label in train_dataloader:
+
+            spectogram = spectogram.to(device)
+            label = label.to(device)
+
+            outputs = model(spectogram)
+
+            loss = crtierion(label, outputs)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            print(f"Batch [{j}/{len(train_dataloader)}], Loss: {loss.item():.4f}")
+            j += 1
+
+
+        print(f"Epoch [{i+1}/{N_ITERATIONS}], Loss: {loss.item():.4f}")
+
+    torch.save(model.state_dict(), "GTZANClassifier")
